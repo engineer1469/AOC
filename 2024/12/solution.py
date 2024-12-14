316ad47d79c8da67e4b750b2a4ac08e478e8c855
Sepp Beld <seppbeld@gmail.com>
2024-12-14 15:01:27 +0100

    FINALLY DAY12

--- 2024/12/solution.py
+++ 2024/12/solution.py
@@ -13,8 +13,7 @@ def findRegions(data):
     width = len(data[0]) if height > 0 else 0
     visited = set()
     regions = []
-    
-    directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+    directions = [(-1,0),(1,0),(0,-1),(0,1)]
     
     for y in range(height):
         for x in range(width):
@@ -34,9 +33,7 @@ def findRegions(data):
                             if data[ny][nx] == symbol and (nx, ny) not in visited:
                                 visited.add((nx, ny))
                                 queue.append((nx, ny))
-
                 regions.append(region)
-    
     return regions
 
 def part1(data):
@@ -44,120 +41,131 @@ def part1(data):
     regions = findRegions(data)
     height = len(data)
     width = len(data[0]) if height > 0 else 0
-    directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
 
+    directions = [(-1,0),(1,0),(0,-1),(0,1)]
     total_price = 0
-    
     for region in regions:
-        # Determine symbol from any cell in region
         any_point = next(iter(region))
         x0, y0 = any_point
         symbol = data[y0][x0]
 
         area = len(region)
         perimeter = 0
-        
         for (cx, cy) in region:
-            # Check each of the four sides for perimeter contribution
             for dx, dy in directions:
-                nx, ny = cx + dx, cy + dy
-                if not (0 <= nx < width and 0 <= ny < height and data[ny][nx] == symbol and (nx, ny) in region):
+                nx, ny = cx+dx, cy+dy
+                if not (0 <= nx < width and 0 <= ny < height and (nx, ny) in region and data[ny][nx] == symbol):
                     perimeter += 1
-
         price = area * perimeter
         total_price += price
-
     return total_price
 
-def getCellEdges(region, data, x, y):
+N, E, S, W = 'N','E','S','W'
+
+def getEdges(region, data):
+    edges = set()
     height = len(data)
     width = len(data[0]) if height > 0 else 0
-    
-    edges = []
-    
-    # Top edge
-    if y == 0 or (x, y-1) not in region:
-        edges.append(('H', y, x))
-    # Bottom edge
-    if y == height - 1 or (x, y+1) not in region:
-        edges.append(('H', y+1, x))
-    # Left edge
-    if x == 0 or (x-1, y) not in region:
-        edges.append(('V', x, y))
-    # Right edge
-    if x == width - 1 or (x+1, y) not in region:
-        edges.append(('V', x+1, y))
-    
+
+    for (x, y) in region:
+        # Check north
+        if y == 0 or (x, y-1) not in region:
+            edges.add((x, y, N))
+        # Check south
+        if y == height - 1 or (x, y+1) not in region:
+            edges.add((x, y, S))
+        # Check west
+        if x == 0 or (x-1, y) not in region:
+            edges.add((x, y, W))
+        # Check east
+        if x == width - 1 or (x+1, y) not in region:
+            edges.add((x, y, E))
+
     return edges
 
-def findHorizontalSides(horizontal_edges):
-    sides = 0
-    # Group by y
-    by_y = defaultdict(list)
-    for (_, y, x) in horizontal_edges:
-        by_y[y].append(x)
-    
-    for y, xs in by_y.items():
-        xs.sort()
-        # Count contiguous runs in xs
-        start = xs[0]
-        prev = xs[0]
-        for i in range(1, len(xs)):
-            if xs[i] != prev + 1:
-                # Gap found, one side ended
-                sides += 1
-                start = xs[i]
-            prev = xs[i]
-        # End the last run
-        sides += 1
-    return sides
+def followPolygon(edges, size):
+    edges = set(edges)
+    sides_count = 0
 
-def findVerticalSides(vertical_edges):
-    sides = 0
-    # Group by x
-    by_x = defaultdict(list)
-    for (_, x, y) in vertical_edges:
-        by_x[x].append(y)
-    
-    for x, ys in by_x.items():
-        ys.sort()
-        # Count contiguous runs in ys
-        start = ys[0]
-        prev = ys[0]
-        for i in range(1, len(ys)):
-            if ys[i] != prev + 1:
-                # Gap found, one side ended
-                sides += 1
-                start = ys[i]
-            prev = ys[i]
-        # End the last run
-        sides += 1
-    return sides
+    while edges:
+        start = next(iter(edges))
+        (x, y, facing) = start
+        first = True
+        loop_start = (x, y, facing)
+        while True:
+            if first:
+                first = False
+            else:
+                if (x, y, facing) in edges:
+                    edges.remove((x, y, facing))
+            if facing == N:
+                if x > 0 and (x-1, y, N) in edges:
+                    x -= 1
+                elif x > 0 and y > 0 and (x-1, y-1, E) in edges:
+                    sides_count += 1
+                    x -= 1
+                    y -= 1
+                    facing = E
+                elif (x, y, W) in edges:
+                    sides_count += 1
+                    facing = W
+                else:
+                    break
+
+            elif facing == E:
+                if y > 0 and (x, y-1, E) in edges:
+                    y -= 1
+                elif y > 0 and x < size - 1 and (x+1, y-1, S) in edges:
+                    sides_count += 1
+                    x += 1
+                    y -= 1
+                    facing = S
+                elif (x, y, N) in edges:
+                    sides_count += 1
+                    facing = N
+                else:
+                    break
+
+            elif facing == S:
+                if x < size - 1 and (x+1, y, S) in edges:
+                    x += 1
+                elif x < size - 1 and y < size - 1 and (x+1, y+1, W) in edges:
+                    sides_count += 1
+                    x += 1
+                    y += 1
+                    facing = W
+                elif (x, y, E) in edges:
+                    sides_count += 1
+                    facing = E
+                else:
+                    break
+
+            elif facing == W:
+                if y < size - 1 and (x, y+1, W) in edges:
+                    y += 1
+                elif y < size - 1 and x > 0 and (x-1, y+1, N) in edges:
+                    sides_count += 1
+                    x -= 1
+                    y += 1
+                    facing = N
+                elif (x, y, S) in edges:
+                    sides_count += 1
+                    facing = S
+                else:
+                    break
+
+    return sides_count
 
 def part2(data):
     data = copy.deepcopy(data)
     regions = findRegions(data)
     total_price = 0
-    
+    height = len(data)
+    size = height
+
     for region in regions:
-        # Collect edges for this region
-        horizontal_edges = set()
-        vertical_edges = set()
-        
-        for (x, y) in region:
-            cell_edges = getCellEdges(region, data, x, y)
-            for e in cell_edges:
-                if e[0] == 'H':
-                    horizontal_edges.add(e)
-                else:
-                    vertical_edges.add(e)
-        
-        h_sides = findHorizontalSides(horizontal_edges)
-        v_sides = findVerticalSides(vertical_edges)
-        
-        sides = h_sides + v_sides
-        
-        # Price = area * sides
+        edges = getEdges(region, data)
+        sides = followPolygon(edges, size)
         area = len(region)
         price = area * sides
         total_price += price
