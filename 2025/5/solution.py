192761526abf357c928816ce5251f0c9b7ef6a6f
Sepp Beld <seppbeld@gmail.com>
2025-12-05 06:55:37 +0100

    day 5

--- 2025/5/solution.py
+++ 2025/5/solution.py
@@ -1,26 +1,58 @@
 import time
 
 def digest():
-    with open('2024/10/input.txt', 'r') as f:
-        data = f.read().splitlines()
-        data = [[int(char) for char in line] for line in data]
-    return data # List[List[int]], 2D list of integers
+    with open('2025/5/input.txt', 'r') as f:
+        input = f.read().split('\n\n')
+        ranges = input[0].splitlines()
+        database = []
+        for r in ranges:
+            start, end = r.split('-')
+            # Create a ranges list of tuples (begin, end)
+            for range in ranges:
+                start, end = range.split('-')
+                database.append((int(start), int(end)))
 
-def part1(data):
-    pass
+        ids = input[1].splitlines()
+        ids = [int(x) for x in ids]
 
-def part2(data):
-    pass
+    return database, ids
+
+def merge_ranges(ranges):
+    sorted_ranges = sorted(ranges, key=lambda x: x[0])
+    merged = []
+    for range in sorted_ranges:
+        if not merged or merged[-1][1] < range[0]:
+            merged.append(range)
+        else:
+            merged[-1] = (merged[-1][0], max(merged[-1][1], range[1]))
+    return merged
+
+def part1(database, ids):
+    count = 0
+    for id in ids:
+        for start, end in database:
+            if start <= id <= end:
+                count += 1
+                break
+    return count
+
+def part2(database):
+    count = 0
+    for range in database:
+        start, end = range
+        count += (end - start + 1)
+    return count
 
 def main():
-    data = digest()
+    database, ids = digest()
+    database = merge_ranges(database)
     st = time.time()
-    result_part1 = part1(data)
+    result_part1 = part1(database, ids)
     print("part1:", result_part1)
     print("time:", time.time() - st)
 
     st = time.time()
-    result_part2 = part2(data)
+    result_part2 = part2(database)
     print("part2:", result_part2)
     print("time:", time.time() - st)
 
